5a2f7b5dca7eb66ef68d94714706774ebf9f417e
namanci <namanci@gmail.com>
2023-11-12 21:39:09 -0300

    Desafío 2

--- ProductManager.js
+++ ProductManager.js
@@ -19,6 +19,7 @@ class ProductManager {
         return JSON.parse(data)
     }
 
+    // Método para agregar un producto.
     async addProduct({ title, description, price, thumbnail, code, stock }) {
         const products = await this.getProducts()
         const id = products.length > 0 ? products[products.length - 1].id + 1 : 1 //id autoincrementable.
@@ -35,19 +36,37 @@ class ProductManager {
         const newProduct = { id, title, description, price, thumbnail, code, stock }
         products.push(newProduct)
         console.log(`El Producto "${newProduct.title}" fue agregado exitosamente.`)
-        const productsString = JSON.stringify(products)
-        await fs.promises.writeFile(this.path, productsString)
+        await fs.promises.writeFile(this.path, JSON.stringify(products))
     }
 
+    // Método para buscar un producto por id.
     async getProductById(id) {
         const products = await this.getProducts()
-        const product = products.find(product => product.id === id)
-        if (!product) { return console.log("Producto no encontrado.") }
+        const product = products.find(product => product.id === id) // Mediante el método find buscamos el producto con el id ingresado.
+        if (!product) return console.log("Producto no encontrado.")
         return product
     }
 
+    // Método para actualizar un producto ingresando su id y un objeto con el campo o campos actualizados.
+    async updateProduct(id, productUpdate) {
+        const products = await this.getProducts()
+        const productFind = products.findIndex(product => product.id === id) // Mediante el método findIndex buscamos el índice o posición del producto en el array.
+        if (productFind === -1) return console.log('No se encuentra el producto.')
+        products[productFind] = { ...products[productFind], ...productUpdate, id } // Actualizamos el producto con el índice obtenido y copiamos mediante spread operator los campos del producto original, luego las actualizaciones y por último el id para que el mismo nunca cambie (incluso si se incluye en productUpdate).
+        await fs.promises.writeFile(this.path, JSON.stringify(products))
+        console.log('Producto actualizado correctamente.')
+    }
 
+    // Método para borrar un producto por su id.
+    async deleteProduct(id) {
+        const products = await this.getProducts()
+        if (!products.some(product => product.id === id)) return console.log('El producto que quiere borrar no existe.') // Mediante el método some comprobamos si el producto existe en el array.
+        const productsFiltered = products.filter(product => product.id !== id) // Mediante el método filter creamos un array con todos los productos menos aquel con el id ingresado.
+        await fs.promises.writeFile(this.path, JSON.stringify(productsFiltered))
+        console.log('Producto eliminado.')
+    }
 }
+
 
 // TESTING
 async function test() {
@@ -57,18 +76,30 @@ async function test() {
     console.log(await instance.getProducts()) // []
 
     await instance.addProduct({
-        title: "Producto Prueba 1",
-        description: "Este es un producto de prueba",
+        title: "producto prueba",
+        description: "Este es un producto prueba",
         price: 200,
         thumbnail: "Sin imagen",
-        code: "abc456",
+        code: "abc123",
         stock: 25
-    }) // El Producto "Producto Prueba 1" fue agregado exitosamente.
+    }) // El Producto "producto prueba" fue agregado exitosamente.
 
-    console.log(await instance.getProducts()) // Muestra el producto recién agregado.
+    console.log('Producto recién agregado:', await instance.getProducts()) // Muestra el producto recién agregado.
 
-    console.log(await instance.getProductById(1)) // Muestra el producto con id = 1.
+    console.log('El producto con id 1 es:', await instance.getProductById(1)) // Muestra el producto con id = 1.
 
+    console.log(await instance.getProductById(99)) // Producto no encontrado.
+
+    await instance.updateProduct(
+        1,
+        {
+            stock: 200
+        }
+    ) // Producto actualizado correctamente.
+
+    await instance.deleteProduct(1) // Producto eliminado.
+
+    await instance.deleteProduct(150) // El producto que quiere borrar no existe.
 }
 
-test()
+test() // Al finalizar la ejecución products.json quedará con un array vacío [].
